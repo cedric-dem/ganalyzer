9fcf6d18396376e1504c34f4b9e8114f77ce84a0
cedric <demuelenaere.cedric@gmail.com>
2025-05-30 14:44:14 +0200

    create displayStats

--- displayStats.py
+++ displayStats.py
@@ -1,2 +1,25 @@
 from config import *
-#TODO
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(statistics_file_path)
+
+groups = {
+    "meds mean": ["median_real","median_fake","mean_real","mean_fake"],
+    "losses": ["gen_loss","disc_loss"],
+    "time":["time"]
+}
+
+for titre, column in groups.items():
+    plt.figure(figsize=(8, 5))
+    for col in column:
+        plt.plot(df[col], label=col)
+    plt.title(f"stats for : {titre}")
+    plt.xlabel("Index")
+    plt.ylabel("quantity")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
