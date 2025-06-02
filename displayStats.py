8a0ec1bce131b6328634a73425035bb605f24cd0
cedric <demuelenaere.cedric@gmail.com>
2025-06-02 13:28:48 +0200

    add unit in display stats script

--- displayStats.py
+++ displayStats.py
@@ -3,13 +3,13 @@ from config import *
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def display_plot(dataframe, title, columns_to_display):
+def display_plot(dataframe, title, columns_to_display, unit):
     plt.figure(figsize=(8, 5))
     for col in columns_to_display:
         plt.plot(dataframe[col], label=col)
     plt.title(f"stats for : {title}")
     plt.xlabel("Index")
-    plt.ylabel("quantity")
+    plt.ylabel(unit)
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
@@ -17,7 +17,7 @@ def display_plot(dataframe, title, columns_to_display):
 
 df = pd.read_csv(statistics_file_path)
 
-display_plot(df, "median/mean",["median_real","median_fake","mean_real","mean_fake"])
-display_plot(df, "losses",["gen_loss","disc_loss"])
-display_plot(df, "time",["time"])
+display_plot(df, "median/mean",["median_real","median_fake","mean_real","mean_fake"], "output")
+display_plot(df, "losses",["gen_loss","disc_loss"], "loss")
+display_plot(df, "time",["time"], "seconds")
 
