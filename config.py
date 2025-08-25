7452bc5b2d04cd37a5c4193b8790f1a6fe7ea107
cedric <demuelenaere.cedric@gmail.com>
2025-08-25 22:50:02 +0200

    clean config + create compare statistics of model

--- config.py
+++ config.py
@@ -1,26 +1,29 @@
 import os
 
-dataset_name = "humans_1"
+#common
+dataset_name = "humans_fifa" #"cars_2"
 dataset_dimension="64"
+dataset_path = "datasets/"+dataset_name+"/"+dataset_dimension
 
-##############################
-
-model_name = "model_a"
-
-model_path = "models/" + dataset_name+"/"+dataset_dimension + "/" +model_name + "/"
+model_name = "model_c"
+global_path = "models/" + dataset_name+"/"+dataset_dimension + "/"
+model_path = global_path + model_name + "/"
 
 os.makedirs(model_path, exist_ok=True)
 
-statistics_file_path = model_path + "statistics.csv"
-
 rgb_images = True
-
 latent_dimension_generator = 121
 
-batch_size = 32
+statistics_file_path = model_path + "statistics.csv"
 
+#train
+batch_size = 32
 save_train_epoch_every = 1
 
+#GUI
 load_quantity_gui = 15
 
-dataset_path = "datasets/"+dataset_name+"/"+dataset_dimension
+#statistics
+show_every_models_statistic = True
+all_models = os.listdir(global_path)
+every_models_statistics_path = [global_path + i for i in all_models]
--- displayStats.py
+++ displayStats.py
@@ -1,14 +1,24 @@
+
 from config import *
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def display_plot(dataframe, title, columns_to_display, unit):
+def display_plot(dataframe, names, title, columns_to_display, unit):
+
     plt.figure(figsize=(8, 5))
-    for col in columns_to_display:
-        plt.plot(dataframe[col], label=col)
-    plt.title(f"stats for : {title}")
+
+    if len(dataframe) == 1:
+        for col in columns_to_display:
+            plt.plot(dataframe[0][col], label=col)
+        plt.title(f"stats for : {title} on model {names[0]}")
+    else:
+        for col in columns_to_display:
+            for i in range (len(dataframe)):
+                plt.plot(dataframe[i][col], label=col+" for "+names[i])
+
+        plt.title(f"stats for : {title} on all models")
     plt.xlabel("Index")
     plt.ylabel(unit)
     plt.legend()
@@ -16,9 +26,16 @@ def display_plot(dataframe, title, columns_to_display, unit):
     plt.tight_layout()
     plt.show()
 
+if show_every_models_statistic:
+    all_dataframes = [pd.read_csv(this_folder_path+"/statistics.csv") for this_folder_path in every_models_statistics_path]
 
-df = pd.read_csv(statistics_file_path)
+    display_plot(all_dataframes, all_models,"median", ["median_real", "median_fake"], "output")
+    display_plot(all_dataframes, all_models,"mean", ["mean_real", "mean_fake"], "output")
+    display_plot(all_dataframes, all_models, "losses", ["gen_loss", "disc_loss"], "loss")
+    display_plot(all_dataframes, all_models, "time", ["time"], "seconds")
 
-display_plot(df, "median/mean", ["median_real", "median_fake", "mean_real", "mean_fake"], "output")
-display_plot(df, "losses", ["gen_loss", "disc_loss"], "loss")
-display_plot(df, "time", ["time"], "seconds")
+else:
+    current_model_dataframe = [pd.read_csv(statistics_file_path)]
+    display_plot(current_model_dataframe, [model_name],"median/mean", ["median_real", "median_fake", "mean_real", "mean_fake"], "output")
+    display_plot(current_model_dataframe,[model_name], "losses", ["gen_loss", "disc_loss"], "loss")
+    display_plot(current_model_dataframe,[model_name], "time", ["time"], "seconds")
